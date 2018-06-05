2e9cbb96b5bd785a22a8fd9b48a03086528ee3c4
Distil62 <Distil62@github.com>
2018-06-05 18:11:30 +0200

    * master Ajout du check mail dans /sql/user.sql + changement de l'ordre des colones lors de la creation des tables.

--- scripts/sql/show.sql
+++ scripts/sql/show.sql
@@ -1,6 +1,6 @@
-CREATE TABLE  show (
-  show_id INTEGER CONSTRAINT show_key PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+CREATE TABLE show (
+  show_id integer CONSTRAINT show_key PRIMARY KEY,
+  name VARCHAR(255),
   description TEXT,
   realease_year INT CHECK (realease_year <= date_part('year', CURRENT_DATE)),
   genre VARCHAR(255),
