60b66eaa59935bc0a55d05e2423971d337672b3f
Distil62 <Distil62@github.com>
2018-06-06 15:57:06 +0200

    * master backup folder bug fixed

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,12 @@
 
 BACKUP_PATH=$(pwd)/backup;
 
+
+if [[ ! -d backup ]]; then
+        mkdir backup;
+        chmod 777 backup;
+fi
+
 psql shows --username=admin\
         --set=backupPath="'$BACKUP_PATH"\
         -f sql/backupAll.sql\
